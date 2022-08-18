08ac6c28e30ded95ec77153358174361a2ae953e
dracero <diego.racero@gmail.com>
2022-08-17 21:21:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import GltfModel from "./gltf";
+
+const ModelViewer = ({ modelPath, scale = 1, position = [0, 0, 0]}) => {
+  const [action, setAction] = useState(false)
+  const Noaction = () => {
+    return <button onClick={()=>setAction(!action)}>
+                 Click to RUN
+          </button>
+  }
+  const Play = () => {
+    return <button onClick={()=>setAction(!action)}>
+                 Click to STOP
+          </button>
+  }
+
+  return (
+  <div className="wrapper"> 
+  <div className="scene-wrapper"> 
+     {action ? <Play /> : <Noaction />}
+   <Canvas>
+      <ambientLight intensity={0.3} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Suspense fallback={null}>
+        <GltfModel modelPath={modelPath} scale={scale} position={position} action={action}/>
+        <OrbitControls />
+      </Suspense>
+    </Canvas>
+   </div> 
+  </div> 
+  );
+};
+
+export default ModelViewer;
---
+++ src/gltf.jsx
@@ -0,0 +1,32 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useLoader, useFrame } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useAnimations, useGLTF } from "@react-three/drei";
+
+const GltfModel = ({ modelPath, scale = 1, position = [0, 0, 0], action }) => {
+  const group = useRef();
+  const gltf = useLoader(GLTFLoader, modelPath);
+  const {  animations } = useGLTF("/eje.glb");
+  const { actions } = useAnimations(animations, group);
+  useEffect(() => {
+    console.log(actions);
+    if (action){
+      actions.eje.play();
+     } else  {
+      actions.eje.stop();
+     }
+  })
+   // Subscribe this component to the render-loop, rotate the mesh every frame
+  //useFrame((state, delta) => (ref.current.rotation.y += 0.003));
+  return (
+    <>
+     <primitive
+        ref={group}
+        object={gltf.scene}
+        position={position}
+      />
+    </>
+  );
+};
+
+export default GltfModel;
